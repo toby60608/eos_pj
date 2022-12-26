5394bf543b82d94f8e0f3150f8dd8982daaab4a0
Sing <49307975+s78718@users.noreply.github.com>
2022-12-26 10:54:01 +0800

    Delete .mydev.ko.cmd

--- .mydev.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/pi/eos_pj/mydev.ko := ld -r -EL  -maarch64elf -z noexecstack --build-id=sha1  -T scripts/module.lds -o /home/pi/eos_pj/mydev.ko /home/pi/eos_pj/mydev.o /home/pi/eos_pj/mydev.mod.o;  true
